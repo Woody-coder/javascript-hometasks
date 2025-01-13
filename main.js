a4dfb280a7f284b2be84bcf0cb7a03b5b2703e8a
Woody-coder <eu.viacheslav.petrenko@gmail.com>
2025-01-13 15:51:09 +0100

    mathematics

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-import "./hello-world/task-1.js";
+// import "./hello-world/task-1.js";
+import "./mathematics/task-1.js";
+import "./mathematics/task-2.js";
+import "./mathematics/task-3.js";
+import "./mathematics/task-4.js";
+

260febde3b04b46780535f99f7ab748e7dfc01a8
Woody-coder <eu.viacheslav.petrenko@gmail.com>
2025-01-15 22:25:51 +0100

    nullish-coalescing-operator

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import "./mathematics/task-1.js";
-import "./mathematics/task-2.js";
-import "./mathematics/task-3.js";
-import "./mathematics/task-4.js";
-import "./if-else/task-5.js";
+// import "./mathematics/task-1.js";
+// import "./mathematics/task-2.js";
+// import "./mathematics/task-3.js";
+// import "./mathematics/task-4.js";
+// import "./if-else/task-5.js";

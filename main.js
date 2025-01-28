69937d55031c5285619aefb806f8b04a6d6bed63
Woody-coder <eu.viacheslav.petrenko@gmail.com>
2025-01-28 15:56:19 +0100

    function

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-import "./mathematics/task-1.js";
-import "./mathematics/task-2.js";
-import "./mathematics/task-3.js";
-import "./mathematics/task-4.js";
-import "./if-else/task-5.js";
-import "./logical-operators/task-2.js";
-import "./logical-operators/task-3.js";
-import "./logical-operators/task-5.js";
-import "./cycle/task-1.js"
-import "./cycle/task-2.js"
-import "./cycle/task-3.js"
-import "./cycle/task-4.js"
-import "./cycle/task-5.js";
-import "./cycle/task-6.js";
+// import "./mathematics/task-1.js";
+// import "./mathematics/task-2.js";
+// import "./mathematics/task-3.js";
+// import "./mathematics/task-4.js";
+// import "./if-else/task-5.js";
+// import "./logical-operators/task-2.js";
+// import "./logical-operators/task-3.js";
+// import "./logical-operators/task-5.js";
+// import "./cycle/task-1.js"
+// import "./cycle/task-2.js"
+// import "./cycle/task-3.js"
+// import "./cycle/task-4.js"
+// import "./cycle/task-5.js";
+// import "./cycle/task-6.js";
+import "./function/test-function.js"
